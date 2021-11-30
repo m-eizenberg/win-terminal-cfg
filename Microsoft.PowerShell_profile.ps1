9eba1f112609de3f6a055a1d4cb17b3b2df80ad6
meizenberg <mike.eizenberg@gmail.com>
2021-11-30 00:58:47 -0800

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Set-PoshPrompt -Theme "C:\Users\micha\Documents\WindowsPowerShell\marcduiker.omp.json"
+#oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
